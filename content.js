e1b7b3a2aadf7639eece237946ad3e88725a2baa
gitusp <ususso@gmail.com>
2022-02-09 20:23:36 +0900

    Add ctrl enter preventer

--- content.js
+++ content.js
@@ -1,3 +1,18 @@
 const main = document.createElement("script");
 main.src = chrome.extension.getURL("main.js");
 document.body.appendChild(main);
+
+chrome.storage.sync.get(
+  {
+    disableCtrlEnter: false,
+  },
+  (items) => {
+    if (items.disableCtrlEnter) {
+      document.body.addEventListener("keydown", (e) => {
+        if (e.code === "Enter" && (e.metaKey || e.ctrlKey)) {
+          e.stopPropagation();
+        }
+      });
+    }
+  }
+);
